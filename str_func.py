fe86a3a7d46d95ce0faaac8f9a7335af26b564ae
Olga Aukhadieva <sarole4ka@gmail.com>
2023-12-22 16:46:18 +0500

    Changes str_func

--- str_func.py
+++ str_func.py
@@ -1,8 +1,8 @@
-def str_func(string):
+def str_func(data):
     """
     Функция, которая принимает на вход строку и возвращает ее со всеми заглавными буквами.
     """
-    return string.upper()
+    return data.upper()
 
 
 def str_func2(string):
